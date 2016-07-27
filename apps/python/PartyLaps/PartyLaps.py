07dda6a851b1e6d1b488611ac8ee38dff03698aa
Rob Haswell <me@robhaswell.co.uk>
2016-07-27 15:05:50 +0100

    Correct positional co-ordinates.

--- apps/python/PartyLaps/PartyLaps.py
+++ apps/python/PartyLaps/PartyLaps.py
@@ -1406,7 +1406,9 @@ def cycleDriver(drivers, currentDriver):
             returnNow = True
     return drivers[0]
 
-def onClickDriver():
+def onClickDriver(a1, a2):
+    ac.log(a1)
+    ac.log(a2)
     global currentDriver
     currentDriver = cycleDriver(driversList, currentDriver)
     writeParameters()
