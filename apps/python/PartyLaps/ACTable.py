e30d36e0b664a756721501fbf5f92a2153001586
Rob Haswell <me@robhaswell.co.uk>
2016-08-09 14:34:13 +0100

    Calculate cell positions and test the first cell.

--- apps/python/PartyLaps/ACTable.py
+++ apps/python/PartyLaps/ACTable.py
@@ -4,23 +4,38 @@ A table drawing utility for Assetto Corsa.
 
 class ACTable(object):
 
-    def __init__(self, ac, nColumns, nRows):
+    def __init__(self, ac, window, nColumns, nRows):
         self.ac = ac
+        self.window = window
         self.paddingX = 0
         self.paddingY = 0
         self.nColumns = nColumns
         self.nRows = nRows
 
-        self._initDataArray()
+        self.cells = [[]]
+
+        self._initialise()
 
 
-    def _initDataArray(self):
+    def _initialise(self):
         """
-        Initialise the data storage array. We are required to do this so that
-        the cell information can be stored when redrawing due to a font size
-        change.
+        Initialise the data storage array and label array. We are required to
+        store cell data so that the cell information can be retrieved when
+        redrawing due to a font size change.
         """
         self.data = [[None]*self.nColumns]*self.nRows
+
+        if self.ac is None:
+            return
+
+        # Delete all existing labels
+        for row in self.cells:
+            for label in row:
+                self.ac.removeLabel(label)
+
+        self.cells = [
+                [ self.ac.addLabel(self.window, 0) for i in xrange(self.nColumns) ]
+            for j in xrange(self.nRows) ]
 
 
     def setFontSize(self, fontSize):
@@ -32,7 +47,7 @@ class ACTable(object):
         Set the pixel amount of padding at the top and left of the table.
         """
         self.paddingX = paddingX
-        self.padding = paddingY
+        self.paddingY = paddingY
 
 
     def setCellSpacing(self, spacing):
@@ -46,8 +61,27 @@ class ACTable(object):
         """
         Set the width of each column. The width is given in multiples of the font size.
         """
+        if len(columnWidths) != self.nColumns:
+            raise ValueError("The number of provided column width entries does "
+                    "not match the expected number of columns.")
         self.columnWidths = columnWidths
 
 
-    def setColumnAlignments(self, columnAlignments):
+    def setColumnAlignments(self, *columnAlignments):
+        """
+        Set the alignments of each column, possible values are 'left', 'right'
+        and 'center'.
+        """
+        if len(columnAlignments) != self.nColumns:
+            raise ValueError("The number of provided column alignment entries "
+                    "does not match the expected number of columns.")
         self.columnAlignments = columnAlignments
+
+
+    def _cellPosition(self, iCol, iRow):
+        """
+        Return the (x,y) co-ordinates for a cell at position iCol,iRow.
+        """
+        x = self.paddingX + (sum(self.columnWidths[:iCol]) * self.fontSize) + (iCol * self.spacing)
+        y = self.paddingY + (iRow * self.fontSize) + (max(0, iRow-1) * self.spacing)
+        return (x, y)
--- apps/python/PartyLaps/test_ACTable.py
+++ apps/python/PartyLaps/test_ACTable.py
@@ -12,7 +12,7 @@ class TestInit(unittest.TestCase):
         """
         We can initialise a 1x1 data array.
         """
-        table = ACTable(None, 1, 1)
+        table = ACTable(None, None, 1, 1)
         self.assertEqual(table.data, [[None]])
 
 
@@ -20,10 +20,32 @@ class TestInit(unittest.TestCase):
         """
         We can initialise a 3x4 data array.
         """
-        table = ACTable(None, 3, 4)
+        table = ACTable(None, None, 3, 4)
         self.assertEqual(table.data, [
             [None, None, None],
             [None, None, None],
             [None, None, None],
             [None, None, None]])
 
+
+
+class TestCellPositions(unittest.TestCase):
+    """
+    Tests for cell positioning.
+    """
+
+    def setUp(self):
+        """
+        Make a 3x4 table with 5+5 padding and 3+3 spacing. The three column
+        widths are 5, 6, and 7.
+        """
+        self.table = ACTable(None, None, 3, 4)
+        self.table.setTablePadding(5, 5)
+        self.table.setCellSpacing(3)
+        self.table.setColumnWidths(5, 6, 7)
+        self.table.setFontSize(18)
+
+    def testTopLeft(self):
+        result = self.table._cellPosition(0, 0)
+        expected = (5, 5)
+        self.assertEqual(result, expected)
